125c8bf57ed42b69b06379ed7f7d4ab34079a33e
Execise <vajarinkarapurva>
2023-02-07 16:51:55 +0530

    test

--- exc4.py
+++ exc4.py
@@ -6,6 +6,7 @@ cars_not_driven = cars -drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
+print("hello")
 
 print ("There are", cars, "cars available.")
 print ("There are only", drivers, "drivers available.")
